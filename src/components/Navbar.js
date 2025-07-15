eb323aa097e0b2b8ec1e77221fd4c8d30fcb4313
RShivasurya201 <151813558+RShivasurya201@users.noreply.github.com>
2025-07-15 09:34:47 +0530

    -

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav style={{ padding: "10px", background: "#f0f0f0" }}>
-      <a href="/" style={{ marginRight: "10px" }}>Home</a>
-      <a href="/about">About</a>
-      <a href="/contact" style={{ marginLeft: "10px" }}>Contact</a>
-    </nav>
-  );
-}
