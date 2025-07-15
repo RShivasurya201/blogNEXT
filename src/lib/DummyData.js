89c724ae4e30faaa314d71148bf770aaf8592282
RShivasurya201 <151813558+RShivasurya201@users.noreply.github.com>
2025-07-15 09:34:33 +0530

    -

--- src/lib/DummyData.js
+++
@@ -1,5 +0,0 @@
-
-export const posts = [
-  { slug: "nextjs-routing-guide", title: "Next.js Routing Guide", content: "Routing in Next.js is powerful..." },
-  { slug: "how-to-bulk-up", title: "How to Bulk Up", content: "Eat big, lift big, sleep big..." },
-];
